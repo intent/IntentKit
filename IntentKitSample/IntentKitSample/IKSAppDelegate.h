bb61fda2f10ec84575f909b8a71509f12093e4c1
Jonathan Dalrymple <jonathan@float-right.co.uk>
2012-07-21 14:18:54 -0700

    Added the IntentKitSample

---
+++ IntentKitSample/IntentKitSample/IKSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  IKSAppDelegate.h
+//  IntentKitSample
+//
+//  Created by Jonathan Dalrymple on 21/07/2012.
+//  Copyright (c) 2012 Intent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class IKSViewController;
+
+@interface IKSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) IKSViewController *viewController;
+
+@end
---
+++ IntentKitSample/IntentKitSample/IKSViewController.m
@@ -0,0 +1,45 @@
+//
+//  IKSViewController.m
+//  IntentKitSample
+//
+//  Created by Jonathan Dalrymple on 21/07/2012.
+//  Copyright (c) 2012 Intent. All rights reserved.
+//
+
+#import "IKSViewController.h"
+#import "IntentKit.h"
+
+@interface IKSViewController ()
+
+@end
+
+@implementation IKSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)showPicker:(id)sender {
+	
+	ISKIntentPickerViewController *picker = [[ISKIntentPickerViewController alloc] initWithIntent:nil];
+	
+	[self presentViewController:picker
+					   animated:YES
+					 completion:nil];
+	
+}
+
+@end
