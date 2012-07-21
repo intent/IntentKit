b5157d5cee0f2e84392c05466f922312aec30242
Jonathan Dalrymple <jonathan@float-right.co.uk>
2012-07-21 14:20:41 -0700

    Added Sample app to the workspace

--- IntentKit/IntentKit/ISKIntentListViewController.m
+++ IntentKit/IntentKit/ISKIntentListViewController.m
@@ -32,24 +32,26 @@
 {
     [super viewDidLoad];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+	UIBarButtonItem *cancelBtn = [[UIBarButtonItem alloc] initWithTitle:@"Cancel"
+																  style:UIBarButtonItemStyleDone
+																 target:self
+																 action:@selector(didSelectCancel)];
+	
+	[[self navigationItem] setRightBarButtonItem:cancelBtn];
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
+
+#pragma mark
 
 #pragma mark - Table view data source
 
